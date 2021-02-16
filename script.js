b30c20d89545b26d6bb3ec673dc7a224b1afa36f
Sebabatso Mashego <52962008+Sebabatso-M@users.noreply.github.com>
2021-02-16 15:10:19 +0200

    added support for desktop view

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const shareBtn = document.getElementById("share-icon-bg");
 const linksSection = document.getElementById("share-links");
 const profileSection = document.getElementById("profile");
 const bottomSection = document.getElementById("bottom-section");
+const arrowDown = document.getElementById("arrow-down");
 
 shareBtn.addEventListener('click', () => {
-    profileSection.classList.toggle("hidden");
-    linksSection.classList.toggle("hidden");
-    bottomSection.classList.toggle("active");
+    if (window.screen.width < 768) {
+        profileSection.classList.toggle("hidden");
+        linksSection.classList.toggle("hidden");
+        bottomSection.classList.toggle("active");
+    }else{
+        linksSection.classList.toggle("hidden");
+        arrowDown.classList.toggle("hidden");
+    } 
+    
 });
